340ed8fa6b450854d6d6acc9e7d3a1b4e99c75b6
Ann <anut.kirillova@gmail.com>
2021-09-06 18:00:21 +0300

    Add list with cars

---
+++ CarsSwifrUI/CarsSwifrUI/CarModel.swift
@@ -0,0 +1,8 @@
+//
+//  CarModel.swift
+//  CarsSwifrUI
+//
+//  Created by Ann on 06.09.2021.
+//
+
+import Foundation
---
+++ CarsSwifrUI/CarsSwifrUI/CarsSwifrUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsSwifrUIApp.swift
+//  CarsSwifrUI
+//
+//  Created by Ann on 06.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct CarsSwifrUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
